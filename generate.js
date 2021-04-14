faceb35f6f1d03138e9a3ed32f1178ce09e59589
yasuyuky <yasuyuki.ymd@gmail.com>
2021-04-14 20:54:39 +0900

    Use the dimension of the svg element

--- generate.js
+++ generate.js
@@ -13,7 +13,10 @@ program.arguments("<input> <output>").action(async (input, output) => {
   await page.goto(`file:${path.join(process.cwd(), input)}`);
   await page.waitForLoadState("domcontentloaded");
   await sleep(1000);
-  await page.pdf({ path: path.join(process.cwd(), output) });
+  const svg = await page.$("svg");
+  const width = await svg?.getAttribute("width");
+  const height = await svg?.getAttribute("height");
+  await page.pdf({ path: path.join(process.cwd(), output), width, height });
   await browser.close();
 });
 program.parse();
