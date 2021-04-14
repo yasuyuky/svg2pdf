ede8b3b2d0ceca5774e3e73afed0ab02a67d069a
yasuyuky <yasuyuki.ymd@gmail.com>
2021-04-14 17:30:27 +0900

    Use commander to parse arguments

--- generate.js
+++ generate.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
 
 const { chromium } = require("playwright-chromium");
+const { program } = require("commander");
 const path = require("path");
 const process = require("process");
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-(async () => {
-  const args = process.argv.slice(2);
+program.arguments("<input> <output>").action(async (input, output) => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto(`file:${path.join(process.cwd(), args[0])}`);
+  await page.goto(`file:${path.join(process.cwd(), input)}`);
   await page.waitForLoadState("domcontentloaded");
   await sleep(1000);
-  await page.pdf({ path: path.join(process.cwd(), args[1]) });
+  await page.pdf({ path: path.join(process.cwd(), output) });
   await browser.close();
-})();
+});
+program.parse();
